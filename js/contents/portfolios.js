142de9aa0893f559ba6709ccdade9ff3cb1a9265
Yuriahn <elisabet517@naver.com>
2019-02-24 18:10:52 +0900

    포트폴리오 최초 버전 업로드

---
+++ js/contents/portfolios.js
@@ -0,0 +1,444 @@
+var portfolios_order = ['bt', 'alice', 'mvr', 'hs', 'fi', '9', 'nana6', 'se', 'dove', 'sb']
+
+var portfolios_info = {
+    'abc': {
+
+    },
+    'bt': {
+        'top-img': "background-image: url('./images/P_BT_top.jpg')",
+
+        'tt_obj' : "책의 맛",
+        'rl_obj': "UX/UI design, Branding<br>2019<br>IOS App. Coming soon",
+        'dsc_obj': "<b>Description</b><br>description",
+        'nm_obj': "<b>UX Design:</b> name<br><b>UI Design:</b> name<br><b>Front-end programming:</b> name<br><b>Back-end programming:</b> name",
+
+        'img_obj_1': {
+            'src': './images/P_BT_wireframe.jpg',
+        },
+        'img_obj_2': {
+            'src': './images/P_BT_01.jpg',
+            'style': 'margin-top: 0; width: 100%; margin-left: 0; margin-right: 0;'
+        },
+        'img_obj_3': {
+            'src': './images/P_BT_02.jpg',
+            'style': 'margin-top: -5px; width: 100%; margin-left: 0; margin-right: 0;'
+        },
+        'img_obj_4': {
+            'src': './images/P_BT_03.jpg',
+            'style': 'margin-top: -5px; width: 100%; margin-left: 0; margin-right: 0;'
+        },
+        'img_obj_5': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_6': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_7': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_8': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_9': {
+            'src': '',
+            'style': 'display: none;'
+        },
+    },
+ 
+    'alice': {
+        'top-img': "background-image: url('./images/P_alice_top.svg')",
+
+        'tt_obj' : "Alice in Dataland",
+        'rl_obj': "Illustration<br>2018",
+        'dsc_obj': "<b>Description</b><br>description",
+        'nm_obj': "<b>Design:</b> name",
+
+        'img_obj_1': {
+            'src': './images/P_alice_01.svg',
+            'style': 'margin-top: 100px; width: 100%; margin-left: 0; margin-right: 0;'
+        },
+        'img_obj_2': {
+            'src': './images/P_alice_02.svg',
+        },
+        'img_obj_3': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_4': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_5': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_6': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_7': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_8': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_9': {
+            'src': '',
+            'style': 'display: none;'
+        },
+    },
+
+    'mvr': {
+        'top-img': "background-image: url('./images/P_MVR_top.svg')",
+
+        'tt_obj' : "Mooovr",
+        'rl_obj': "Web, Branding<br>2017<br>",
+        'dsc_obj': "<b>Description</b><br>description",
+        'nm_obj': "<b>Design:</b> name<br><br><a href='http://mooovr.com/' target='_blank'>View Website</a>",
+
+        'img_obj_1': {
+            'src': './images/P_MVR_01.jpg',
+            'style': 'margin-top: 100px;'
+        },
+        'img_obj_2': {
+            'src': './images/P_MVR_02.jpg',
+        },
+        'img_obj_3': {
+            'src': './images/P_MVR_03.jpg',
+        },
+        'img_obj_4': {
+            'src': './images/P_MVR_04.jpg',
+        },
+        'img_obj_5': {
+            'src': './images/P_MVR_05.jpg',
+        },
+        'img_obj_6': {
+            'src': './images/P_MVR_06.jpg',
+        },
+        'img_obj_7': {
+            'src': './images/P_MVR_07.jpg',
+            'style': 'margin-top: -5px;'
+        },
+        'img_obj_8': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_9': {
+            'src': '',
+            'style': 'display: none;'
+        },
+    },
+
+    'hs': {
+        'top-img': "background-image: url('./images/P_HS_top.jpg')",
+
+        'tt_obj' : "Hair schedule",
+        'rl_obj': "UX/UI design, Branding<br>2015",
+        'dsc_obj': "<b>Description</b><br>description",
+        'nm_obj': "<b>Planning:</b> name<br><b>Design:</b> name",
+
+        'img_obj_1': {
+            'src': './images/P_HS_01.svg',
+            'style': 'margin-top:150px;'
+        },
+        'img_obj_2': {
+            'src': './images/P_HS_02.svg',
+            'style': ''
+        },
+        'img_obj_3': {
+            'src': './images/P_HS_03.svg',
+        },
+        'img_obj_4': {
+            'src': './images/P_HS_04.svg',
+        },
+        'img_obj_5': {
+            'src': './images/P_HS_05.svg',
+        },
+        'img_obj_6': {
+            'src': './images/P_HS_06.svg',
+        },
+        'img_obj_7': {
+            'src': './images/P_HS_07.svg',
+        },
+        'img_obj_8': {
+            'src': './images/P_HS_08.svg',
+            'style': 'margin-bottom:200px;'
+        },
+        'img_obj_9': {
+            'src': '',
+            'style': 'display: none;'
+        },
+    },
+
+    'fi': {
+        'top-img': "background-image: url('./images/P_FI_top.jpg')",
+
+        'tt_obj' : "Study on Flexible identity",
+        'rl_obj': "Infographic<br>2015",
+        'dsc_obj': "<b>Description</b><br>description",
+        'nm_obj': "<b>Planning:</b> name<br><b>Design:</b> name",
+
+        'img_obj_1': {
+            'src': './images/P_FI.jpg',
+            'style': 'margin-top:150px; width: 100%; margin-left: 0; margin-right: 0;'
+        },
+        'img_obj_2': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_3': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_4': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_5': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_6': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_7': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_8': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_9': {
+            'src': '',
+            'style': 'display: none;'
+        },
+    },
+
+    '9': {
+        'top-img': "background-image: url('./images/P_9_top.jpg')",
+
+        'tt_obj' : "9 o'clock",
+        'rl_obj': "Branding<br>2012<br><img src='./images/P_9_reddot.svg'>",
+        'dsc_obj': "<b>Description</b><br>description",
+        'nm_obj': "<b>Planning:</b> name<br><b>Design:</b> name",
+
+        'img_obj_1': {
+            'src': './images/P_9_01.jpg',
+            'style': 'margin-top:100px;'
+        },
+        'img_obj_2': {
+            'src': './images/P_9_02.jpg',
+        },
+        'img_obj_3': {
+            'src': './images/P_9_03.jpg',
+        },
+        'img_obj_4': {
+            'src': './images/P_9_04.jpg',
+        },
+        'img_obj_5': {
+            'src': './images/P_9_05.jpg',
+        },
+        'img_obj_6': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_7': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_8': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_9': {
+            'src': '',
+            'style': 'display: none;'
+        },
+    },
+
+    'nana6': {
+        'top-img': "background-image: url('./images/P_nana6_top.jpg')",
+
+        'tt_obj' : "Nana6 project",
+        'rl_obj': "Editorial design<br>2012",
+        'dsc_obj': "<b>Description</b><br>description",
+        'nm_obj': "<b>Planning:</b> name<br><b>Design:</b> name",
+
+        'img_obj_1': {
+            'src': './images/P_nana6_01.jpg',
+            'style': 'margin-top: 150px;'
+        },
+        'img_obj_2': {
+            'src': './images/P_nana6_02.jpg',
+        },
+        'img_obj_3': {
+            'src': './images/P_nana6_03.jpg',
+        },
+        'img_obj_4': {
+            'src': './images/P_nana6_04.jpg',
+        },
+        'img_obj_5': {
+            'src': './images/P_nana6_05.jpg',
+        },
+        'img_obj_6': {
+            'src': './images/P_nana6_06.jpg',
+        },
+        'img_obj_7': {
+            'src': './images/P_nana6_07.jpg',
+        },
+        'img_obj_8': {
+            'src': './images/P_nana6_08.jpg',
+        },
+        'img_obj_9': {
+            'src': './images/P_nana6_09.jpg',
+            'style': 'margin-top: 100px; width: 100%; margin-left: 0; margin-right: 0;'
+        },
+    },
+
+    'se': {
+        'top-img': "background-image: url('./images/P_SE_top.jpg')",
+
+        'tt_obj' : "Save earth",
+        'rl_obj': "Visual artwork, Branding<br>2009",
+        'dsc_obj': "<b>Description</b><br>description",
+        'nm_obj': "<b>Planning:</b> name<br><b>Design:</b> name",
+
+        'img_obj_1': {
+            'src': './images/P_SE_logo.svg',
+        },
+        'img_obj_2': {
+            'src': './images/P_SE_01.jpg',
+            // 'style': 'width: 100%; margin-left: 0%; margin-right: 0%;'
+        },
+        'img_obj_3': {
+            'src': './images/P_SE_02.jpg',
+            // 'style': 'margin-top: -5px; width: 100%; margin-left: 0%; margin-right: 0%;'
+        },
+        'img_obj_4': {
+            'src': './images/P_SE_03.jpg',
+            // 'style': 'margin-top: -5px; width: 100%; margin-left: 0%; margin-right: 0%;'
+        },
+        'img_obj_5': {
+            'src': './images/P_SE_04.jpg',
+            // 'style': 'margin-top: -5px; width: 100%; margin-left: 0%; margin-right: 0%;'
+        },
+        'img_obj_6': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_7': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_8': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_9': {
+            'src': '',
+            'style': 'display: none;'
+        },
+    },
+
+    'dove': {
+        'top-img': "background-image: url('./images/P_dove_featured.jpg')",
+
+        'tt_obj' : "Dove world",
+        'rl_obj': "Visual artwork<br>2007",
+        'dsc_obj': "<b>Description</b><br>description",
+        'nm_obj': "<b>Planning:</b> name<br><b>Design:</b> name",
+
+        'img_obj_1': {
+            // 'src': './images/P_dove_featured.jpg',
+            // 'style': 'margin-top:100px; width: 100%; margin-left: 0; margin-right: 0;'
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_2': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_3': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_4': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_5': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_6': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_7': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_8': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_9': {
+            'src': '',
+            'style': 'display: none;'
+        },
+    },
+
+    'sb': {
+        'top-img': "background-image: url('./images/P_SB_top.svg')",
+
+        'tt_obj' : "Suabow",
+        'rl_obj': "Branding, Character<br>2007",
+        'dsc_obj': "<b>Description</b><br>description",
+        'nm_obj': "<b>Planning:</b> name<br><b>Design:</b> name",
+
+        'img_obj_1': {
+            'src': './images/P_SB_01.svg',
+            'style': 'margin-top: 100px; width: 100%; margin-left: 0; margin-right: 0;'
+        },
+        'img_obj_2': {
+            'src': './images/P_SB_02.svg',
+            'style': 'margin-top: -6px; width: 100%; margin-left: 0; margin-right: 0;'
+        },
+        'img_obj_3': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_4': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_5': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_6': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_7': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_8': {
+            'src': '',
+            'style': 'display: none;'
+        },
+        'img_obj_9': {
+            'src': '',
+            'style': 'display: none;'
+        },
+    },
+}
